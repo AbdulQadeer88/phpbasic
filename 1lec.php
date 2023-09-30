1338def88c7c101623de2cca1edd02de9122572b
AbdulQadeer88 <146485038+AbdulQadeer88@users.noreply.github.com>
2023-09-30 12:16:16 +0500

    Add files via upload

---
+++ 1lec.php
@@ -0,0 +1,24 @@
+<h1>Operator with if else</h1>
+<?php
+$x=24;
+$y=14;
+
+$operater="+ ";
+if($operator="+"){
+echo "The Sum  of  ".$x."+".$y." = ".$x+$y; 
+echo "<br>";
+}
+if($operator="-"){
+echo "The Subtraction  of  ".$x."-".$y." = ".$x-$y;
+echo "<br>";
+}
+if($operator="*"){
+echo "The multiplication  of  ".$x."*".$y." = ".$x*$y;
+echo "<br>";
+}
+if($operator="/"){
+echo "The  divison of  ".$x."/".$y." = ".$x/$y;
+echo "<br>";
+}
+
+?>
